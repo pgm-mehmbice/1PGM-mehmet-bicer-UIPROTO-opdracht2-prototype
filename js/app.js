e5b8c97b890e28dc7f678b5a2dabb8f72245695c
pgm-mehmbice <mehmbice@student.arteveldehs.be>
2021-03-16 14:22:48 +0100

    Start

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// import
+
+const app = {
+  async init() {
+    this.cacheElements();
+    this.registerListeners();
+};
+
+cacheElements() {
+    // this.$ = document.querySelector("");
+    // this.$ = document.querySelector("");
+    // this.$ = document.querySelector("");
+    // this.$ = document.querySelector("");
+}
+
+registerListeners() {
+    this.event 
+}
+
+// beam me up scotty
+app.init();
